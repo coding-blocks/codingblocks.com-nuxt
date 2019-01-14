a9e777c61417e064164a8c879964323c1d624c9f
Arnav Gupta <arnav@codingblocks.com>
2019-01-14 16:58:35 +0530

    basic setup with cb bootstrap
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        codingblocks.com-nuxt
+      </h1>
+      <h2 class="subtitle">
+        Coding Blocks Website
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="btn btn-success">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="btn btn-secondary">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+</style>
