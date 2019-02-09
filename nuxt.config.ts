607af364b761c85a111113b3d6eff38b8e4ca2af
Jatin Katyal <jatin.katyal13@gmail.com>
2019-02-09 20:48:16 +0530

    plugins and config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,62 @@
-const config = {
+export default {
+  mode: 'universal',
+
+  /*
+   ** Headers of the page
+   */
   head: {
-    title: 'starter',
+    title: 'Best programming Courses in Delhi-NCR | Coding Blocks',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt TS project' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Coding Blocks Main Website',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  loading: { color: '#3B8070' },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  /*
+   ** Global CSS
+   */
   css: [
     '@coding-blocks/bootstrap/dist/css/bootstrap.css',
-    '~/assets/css/main.css',
+    'swiper/dist/css/swiper.css',
   ],
-  modules: [['bootstrap-vue/nuxt', { css: false }]],
-}
 
-export default config
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [{ src: '~/plugins/swiper.ts', ssr: false }],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://bootstrap-vue.js.org/docs/
+    ['bootstrap-vue/nuxt', { css: false }],
+    [
+      'nuxt-fontawesome',
+      {
+        component: 'fab',
+        imports: [
+          // import whole set
+          {
+            set: '@fortawesome/free-brands-svg-icons',
+            icons: ['fab'],
+          },
+          {
+            set: '@fortawesome/free-solid-svg-icons',
+            icons: ['fas'],
+          },
+        ],
+      },
+    ],
+  ],
+}
